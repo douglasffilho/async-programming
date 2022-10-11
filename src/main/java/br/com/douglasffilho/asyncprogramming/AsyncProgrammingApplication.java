798b7716891e41f5f55f76b81b030d96da5367d4
Douglas Fernandes <douglasf.filho@gmail.com>
2022-10-11 14:31:25 -0300

    chore: add first project config

---
+++ src/main/java/br/com/douglasffilho/asyncprogramming/AsyncProgrammingApplication.java
@@ -0,0 +1,13 @@
+package br.com.douglasffilho.asyncprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/douglasffilho/asyncprogramming/AsyncProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.douglasffilho.asyncprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
